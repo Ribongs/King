c2e2374ae0b3f7e383ac1abfd2b23a57293234b7
Ribongs <58774123+Ribongs@users.noreply.github.com>
2019-12-11 14:25:03 +0200

    Add files via upload

---
+++ Demo test2 - Storyline output/story_content/user.js
@@ -0,0 +1,15 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6nl7HrEI9OR":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  parent.submitLearnDashLessonComplete();
+}
+
